d0d7a6bee584223b16ad587241cd52954524a004
sainikitha99 <sainikitha.dmm@gmail.com>
2019-03-28 10:52:15 +0530

    initial commit

---
+++ widg/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget{
+  MyAppBar({this.title});
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56.0,
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null,
+          ),
+          Expanded(
+            child: title,
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyScaffold extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Material(
+      child: Column(
+        children: <Widget>[
+          MyAppBar(
+            title: Text(
+              'Example',
+              style: Theme.of(context).primaryTextTheme.title,
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Text('hello world')
+            )
+          )
+        ],
+      ),
+    );
+  }
+}
+
+void main(){
+  runApp(MaterialApp(
+    title: 'My app',
+    home: MyScaffold(),
+  ));
+}
